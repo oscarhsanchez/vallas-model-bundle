caf65e6bcc2433ab20422f5ab299f08cbe0e40a3
Débora <debora.vazquez@gmail.com>
2015-12-16 18:28:19 +0100

    Añadida entity Report

---
+++ Entity/Report.php
@@ -0,0 +1,289 @@
+<?php
+
+namespace Vallas\ModelBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use ESocial\ModelBundle\Entity\GenericEntity;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass="Vallas\ModelBundle\Repository\ReportRepository")
+ * @ORM\Table(name="report")
+ * @ORM\HasLifecycleCallbacks
+ */
+class Report extends GenericEntity
+{
+    public function __construct()
+    {
+        $this->token = GenericEntity::generateNewToken();
+    }
+
+    /**
+     * @ORM\PrePersist
+     * @ORM\PreUpdate
+     */
+    public function preSave()
+    {
+        if($this->created_at == null){
+            $this->created_at = new \DateTime();
+        }
+        $this->updated_at = new \DateTime();
+    }
+
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=255, nullable = true, unique=true)
+     */
+    protected $jasper_report_id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $route;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=255, nullable = true)
+     */
+    protected $parameters;
+
+    /** @ORM\Column(type="datetime", nullable = true) */
+    protected $created_at;
+
+    /** @ORM\Column(type="datetime", nullable = true) */
+    protected $updated_at;
+
+    /** @ORM\Column(type="string", length=100, unique=true) */
+    protected $token;
+
+    /**
+     * @ORM\Column(type="smallint",options={"default" = 1})
+     */
+    protected $active = true;
+
+    public function __toString() {
+        return $this->getName();
+    }
+
+    
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set jasperReportId
+     *
+     * @param string $jasperReportId
+     *
+     * @return Report
+     */
+    public function setJasperReportId($jasperReportId)
+    {
+        $this->jasper_report_id = $jasperReportId;
+
+        return $this;
+    }
+
+    /**
+     * Get jasperReportId
+     *
+     * @return string
+     */
+    public function getJasperReportId()
+    {
+        return $this->jasper_report_id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Report
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set route
+     *
+     * @param string $route
+     *
+     * @return Report
+     */
+    public function setRoute($route)
+    {
+        $this->route = $route;
+
+        return $this;
+    }
+
+    /**
+     * Get route
+     *
+     * @return string
+     */
+    public function getRoute()
+    {
+        return $this->route;
+    }
+
+    /**
+     * Set parameters
+     *
+     * @param string $parameters
+     *
+     * @return Report
+     */
+    public function setParameters($parameters)
+    {
+        $this->parameters = $parameters;
+
+        return $this;
+    }
+
+    /**
+     * Get parameters
+     *
+     * @return string
+     */
+    public function getParameters()
+    {
+        return $this->parameters;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     *
+     * @return Report
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->created_at = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->created_at;
+    }
+
+    /**
+     * Set updatedAt
+     *
+     * @param \DateTime $updatedAt
+     *
+     * @return Report
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updated_at = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updated_at;
+    }
+
+    /**
+     * Set token
+     *
+     * @param string $token
+     *
+     * @return Report
+     */
+    public function setToken($token)
+    {
+        $this->token = $token;
+
+        return $this;
+    }
+
+    /**
+     * Get token
+     *
+     * @return string
+     */
+    public function getToken()
+    {
+        return $this->token;
+    }
+
+    /**
+     * Set active
+     *
+     * @param integer $active
+     *
+     * @return Report
+     */
+    public function setActive($active)
+    {
+        $this->active = $active;
+
+        return $this;
+    }
+
+    /**
+     * Get active
+     *
+     * @return integer
+     */
+    public function getActive()
+    {
+        return $this->active;
+    }
+}
